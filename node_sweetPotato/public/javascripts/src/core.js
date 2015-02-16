24ecd7ed22fd9da55585093dddb5273935809ccc
Garrett- <joshuadmesser@gmail.com>
2015-02-16 18:58:43 -0500

    Added web sockets test example

--- node_sweetPotato/public/javascripts/src/core.js
+++ node_sweetPotato/public/javascripts/src/core.js
@@ -3,7 +3,31 @@
 
   var client = new Faye.Client('http://localhost:8000/ws');
 
-  client.subscribe('/bar', function(response){
+  $(init);
+
+  client.subscribe('/chatroom', function(response){
     console.log('from server', response);
   });
+
+  function init() {
+    var TA = $('#chat');
+
+    $('#form').on('submit', function(e){
+      e.preventDefault()
+      var msgObject = {
+        message: TA.val()
+      };
+
+      console.log('sending to server', msgObject);
+
+      client.publish('/chatroom', msgObject);
+
+      TA.val('');
+
+      return false;
+    });
+
+  }
+
+
 }());
